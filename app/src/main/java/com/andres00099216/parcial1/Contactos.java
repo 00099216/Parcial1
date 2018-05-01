f456b2b0ed9ecef899a75926375aafb75541c0c0
00099216 <32801207+00099216@users.noreply.github.com>
2018-04-30 18:49:31 -0600

    Se agrega el modelo para crear contactos y el layout del contacto para la pagina principal

---
+++ app/src/main/java/com/andres00099216/parcial1/Contactos.java
@@ -0,0 +1,61 @@
+package com.andres00099216.parcial1;
+
+/**
+ * Created by Andres on 30/4/2018.
+ */
+
+public class Contactos {
+private String nombre;
+private String telefono;
+private String correo;
+private boolean agregado;
+private int foto;
+
+    public Contactos(String nombre, String telefono, String correo, boolean agregado, int foto) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.correo = correo;
+        this.agregado = agregado;
+        this.foto = foto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public boolean isAgregado() {
+        return agregado;
+    }
+
+    public void setAgregado(boolean agregado) {
+        this.agregado = agregado;
+    }
+
+    public int getFoto() {
+        return foto;
+    }
+
+    public void setFoto(int foto) {
+        this.foto = foto;
+    }
+}
--- app/src/main/java/com/andres00099216/parcial1/MainActivity.java
+++ app/src/main/java/com/andres00099216/parcial1/MainActivity.java
@@ -2,12 +2,24 @@ package com.andres00099216.parcial1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton favoritos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        favoritos = findViewById(R.id.boton_favoritos);
+
+
     }
+
+
+
 }
