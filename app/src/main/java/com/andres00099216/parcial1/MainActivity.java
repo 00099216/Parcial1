ba477a499ed69b6e790b605e814064aae3277325
00099216 <32801207+00099216@users.noreply.github.com>
2018-05-01 23:38:03 -0600

    se setea el adaptador y se ponen valores quemados en el objeto para hacer pruebas

--- app/src/main/java/com/andres00099216/parcial1/MainActivity.java
+++ app/src/main/java/com/andres00099216/parcial1/MainActivity.java
@@ -2,24 +2,42 @@ package com.andres00099216.parcial1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
-    ImageButton favoritos;
+    private RecyclerView recyclerView;
+    private  RecyclerContactosAdaptor contactosAdaptor;
+    private ArrayList<Contacto> Contactos;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        favoritos = findViewById(R.id.boton_favoritos);
+        recyclerView = findViewById(R.id.recycler_view_contactos);
+
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
+
+        Contactos = new ArrayList<>();
+
+        Contactos.add(new Contacto("Andres", "78192239", "00099216@", R.drawable.generic_picture));
+        Contactos.add(new Contacto("Andres", "78192239", "00099216@", R.drawable.generic_picture));
+        Contactos.add(new Contacto("Andres", "78192239", "00099216@", R.drawable.generic_picture));
+        Contactos.add(new Contacto("Andres", "78192239", "00099216@", R.drawable.generic_picture));
 
 
+
+        contactosAdaptor = new RecyclerContactosAdaptor(Contactos) {
+
+        };
+        recyclerView.setAdapter(contactosAdaptor);
     }
-
-
-
 }
