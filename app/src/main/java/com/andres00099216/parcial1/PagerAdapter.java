e0c443e289c6193248298d932758a456f5772623
00099216 <32801207+00099216@users.noreply.github.com>
2018-05-08 04:08:42 -0600

    Se agrega el pager adapter que hace las dos pestañas, la de contactos y contactos favoritos.

--- app/src/main/java/com/andres00099216/parcial1/PagerAdapter.java
+++ app/src/main/java/com/andres00099216/parcial1/PagerAdapter.java
@@ -1,8 +1,49 @@
 package com.andres00099216.parcial1;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Andres on 6/5/2018.
  */
 
-public class PagerAdapter {
+public class PagerAdapter extends FragmentPagerAdapter {
+    //Variables
+    private int contador= 2;
+    private List<String> titulos= new ArrayList<>();
+    private List<Fragment> fragmento = new ArrayList<>();
+    
+    public PagerAdapter(FragmentManager manager) {
+        super(manager);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position){
+        return titulos.get(position);
+    }
+
+    //Se agrega fragmentos y titulos al Viewpager
+    public void addFragment(Fragment fragment, String title) {
+        fragmento.add(fragment);
+        titulos.add(title);
+    }
+    //Metodos por defecto
+    @Override
+    public Fragment getItem(int position) {
+        return fragmento.get(position);
+    }
+
+    @Override
+    public int getItemPosition(Object object) {
+        return POSITION_NONE;
+    }
+
+    @Override
+    public int getCount() {
+        return contador;
+    }
 }
