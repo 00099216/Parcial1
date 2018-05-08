cb212727a8855dacb645b668d15fad7ca890a626
00099216 <32801207+00099216@users.noreply.github.com>
2018-05-08 03:52:28 -0600

    Se agrega el fragmento FragmentoMostrarContact que muestra el perfil del contacto cuando la orientacion es horizontal

--- app/src/main/java/com/andres00099216/parcial1/FragmentoMostrarContacto.java
+++ app/src/main/java/com/andres00099216/parcial1/FragmentoMostrarContacto.java
@@ -1,8 +1,109 @@
 package com.andres00099216.parcial1;
 
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.support.v4.app.Fragment;
+
+
 /**
  * Created by Andres on 6/5/2018.
  */
 
-public class FragmentoMostrarContacto {
+public class FragmentoMostrarContacto extends Fragment {
+    //Variables
+    private ImageView foto;
+    private TextView nombre;
+    private TextView telefono;
+    private TextView correo;
+    private ImageView llamar;
+    private ImageView compartir;
+    private String path;
+    Contacto C;
+    private static final int PERMISSIONS_REQUEST_PHONE_CALL = 101;
+
+    Contacto contacto;
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragmento_perfil_contacto, container, false);
+
+        foto = view.findViewById(R.id.foto_contacto);
+        nombre = view.findViewById(R.id.nombre_contacto);
+        telefono = view.findViewById(R.id.telefono_contacto);
+        correo = view.findViewById(R.id.correo_contacto);
+
+
+        llamar = view.findViewById(R.id.llamar);
+        //Funcionalidad el boton llamar
+        llamar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Donde se realiza la llamada
+                Intent intent = new Intent(Intent.ACTION_CALL);
+                intent.setData(Uri.parse("tel:" + telefono.getText().toString().replace("-", "")));
+
+                if (ContextCompat.checkSelfPermission(getContext(), android.Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(getActivity(), new String[]{android.Manifest.permission.CALL_PHONE}, PERMISSIONS_REQUEST_PHONE_CALL);
+                } else startActivity(intent);
+
+            }
+        });
+
+        compartir = view.findViewById(R.id.compartir);
+        //Donde se comparte
+        compartir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Esto no funciona xdxdxdx
+                Uri imageUri;
+
+                if (path == "")
+                    imageUri = Uri.parse("android.resource://com.andres00099216.parcial1/drawable/generic_picture.png");
+                else
+                    imageUri = Uri.parse(path);
+
+                Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+                emailIntent.setType("image/*");
+                emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "share");
+                emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, "name" + nombre.getText() + "\n" +
+                        "telefono" + telefono.getText() + "\n" + "correo" + correo.getText());
+                emailIntent.putExtra(Intent.EXTRA_STREAM, imageUri);
+
+                //Se inicia el evento
+                startActivity(Intent.createChooser(emailIntent, "enviando"));
+
+            }
+        });
+
+        Bundle bundle = this.getArguments();
+
+        if (savedInstanceState != null) {
+            //por si cambia de estado
+            contacto = savedInstanceState.getParcelable("Contactos");
+        } else {
+            if (bundle != null) {
+                contacto = bundle.getParcelable("Contactos");
+                foto.setImageURI(Uri.parse(contacto.getFoto()));
+                nombre.setText(contacto.getNombre());
+                telefono.setText(contacto.getTelefono());
+                correo.setText(contacto.getCorreo());
+            }
+        }
+        return view;
+    }
 }
+
+
+
+
+
