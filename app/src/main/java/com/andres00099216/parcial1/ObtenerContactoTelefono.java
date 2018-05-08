7e1caf8cecd65ed9d7be24b7dd8067250f404d10
00099216 <32801207+00099216@users.noreply.github.com>
2018-05-08 04:04:54 -0600

    Se agrega ObtenerContactoTelefono, su proposito es el de obtener contactos de la agenda porque el actual no funciona xd pero aun no se implementa

--- app/src/main/java/com/andres00099216/parcial1/ObtenerContactoTelefono.java
+++ app/src/main/java/com/andres00099216/parcial1/ObtenerContactoTelefono.java
@@ -1,8 +1,56 @@
 package com.andres00099216.parcial1;
 
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.Context;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.ContactsContract;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+
 /**
  * Created by Andres on 8/5/2018.
  */
 
 public class ObtenerContactoTelefono {
+    public ArrayList<Contacto> getContacts(Context context) {
+        ArrayList<Contacto> list = new ArrayList<>();
+        ContentResolver contentResolver = context.getContentResolver();
+        Cursor cursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+        if (cursor.getCount() > 0) {
+            while (cursor.moveToNext()) {
+                String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+                if (cursor.getInt(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)) > 0) {
+                    Cursor cursorInfo = contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
+                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?", new String[]{id}, null);
+                    InputStream inputStream = ContactsContract.Contacts.openContactPhotoInputStream(context.getContentResolver(),
+                            ContentUris.withAppendedId(ContactsContract.Contacts.CONTENT_URI, new Long(id)));
+
+                    Uri person = ContentUris.withAppendedId(ContactsContract.Contacts.CONTENT_URI, new Long(id));
+                    Uri pURI = Uri.withAppendedPath(person, ContactsContract.Contacts.Photo.CONTENT_DIRECTORY);
+
+                    Bitmap photo = null;
+                    if (inputStream != null) {
+                        photo = BitmapFactory.decodeStream(inputStream);
+                    }
+                    while (cursorInfo.moveToNext()) {
+                        Contacto contactos = new Contacto(0, null, null, null, false, null);
+                        contactos.setId(Integer.parseInt(id));
+                        contactos.setNombre(cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)));
+                        contactos.setTelefono(cursorInfo.getString(cursorInfo.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)));
+                        contactos.setFoto(cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.PHOTO_URI)));
+                        list.add(contactos);
+                    }
+
+                    cursorInfo.close();
+                }
+            }
+            cursor.close();
+        }
+        return list;
+    }
 }
